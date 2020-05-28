f634d180e098a7a2518baeb0630057258cb13600
Sascha Adler <sadler@crkn.ca>
2020-05-28 17:27:33 -0600

    Cleans up unused couch functions

--- src/resources/couch.js
+++ src/resources/couch.js
@@ -30,46 +30,23 @@ function handleErrors(res) {
   return;
 }
 
-// 1. fetch a document from a given db
-// 2. given a list of ids and a db, fetch all of the documents
-// 3. given a view and a list of keys, fetch all of the values in the view?
-
 async function getDocument(db, id) {
   let response = await _request([db, encodeURIComponent(id)].join("/"));
   handleErrors(response);
   return await response.json();
 }
 
-async function _getResultFromView(db, ddoc, view, key, includeDocs) {
+async function getDocumentFromView(db, ddoc, view, key) {
   let response = await _request(
     [db, "_design", ddoc, "_view", view].join("/"),
-    { key: JSON.stringify(key), include_docs: includeDocs }
+    { key: JSON.stringify(key), include_docs: true }
   );
   handleErrors(response);
   let data = await response.json();
   if (data.rows.length === 0) {
     throw new NotFoundError("Key lookup failed.");
   }
-  return data.rows[0];
-}
-
-async function getIdFromView(db, ddoc, view, key) {
-  return (await _getResultFromView(db, ddoc, view, key, false)).id;
-}
-
-async function getDocumentFromView(db, ddoc, view, key) {
-  return (await _getResultFromView(db, ddoc, view, key, true)).doc;
-}
-
-async function documentsWithPrefix(db, prefix) {
-  return;
-}
-
-async function documentsByIDs(db, keys) {
-  let result = await _request([db, "_all_docs"].join("/"), {}, "POST", {
-    keys,
-  });
-  return result;
+  return data.rows[0].doc;
 }
 
 async function viewResultsFromKeys(db, ddoc, view, keys) {
@@ -85,9 +62,6 @@ async function viewResultsFromKeys(db, ddoc, view, keys) {
 
 module.exports = {
   getDocument,
-  getIdFromView,
   getDocumentFromView,
-  documentsWithPrefix,
-  documentsByIDs,
   viewResultsFromKeys,
 };
