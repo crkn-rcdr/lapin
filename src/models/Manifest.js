a2d32b0e5cb86d6ec09ccdfe279613ecaf0d493f
DivyaKrishnan26 <dkrishnan@crkn.ca>
2020-08-17 17:21:38 -0400

    Manifest api changes- prototype

--- src/models/Manifest.js
+++ src/models/Manifest.js
@@ -1,14 +1,84 @@
-const { viewResultsFromKeys } = require("../resources/couch");
+const { NotFoundError } = require("../errors");
+const { getDocument, viewResultsFromKeys } = require("../resources/couch");
 const { multiTextValueToSingle } = require("../util");
 
 class Manifest {
   static #DB_NAME = "manifest";
   #id;
+  #slug;
+  #label;
+  #summary;
+  #ocrPdf;
+  #public;
+  #type;
+  #freezeParamaters;
+  #canvases = [];
 
   constructor(id) {
     this.#id = id;
   }
+  get id() {
+    this.#id;
+  }
+  async initialize() {
+    let document;
+    try {
+      document = await getDocument(Manifest.#DB_NAME, this.#id);
+    } catch (error) {
+      throw error.status === 404
+        ? new NotFoundError(`Manifest ${this.#id} not found.`)
+        : error;
+    }
+    this.#slug = document.slug;
+    this.#label = multiTextValueToSingle(document.label);
+    if (document.summary)
+      this.#summary = multiTextValueToSingle(document.summary);
+    this.#ocrPdf = document.ocrPdf;
+    this.#type = document.type;
+    this.#freezeParamaters = document.freezeParamaters;
+  }
+  //loads canvases
+  #loadCanvases = async () => {
+    let rows;
+    try {
+      rows = await viewResultsFromKeys(
+        Manifest.#DB_NAME,
+        "access",
+        "canvases",
+        [this.#id]
+      );
+    } catch (error) {
+      throw error;
+    }
 
+    this.#canvases = rows.map((row) => {
+      return {
+        id: row.id,
+        slug: row.value.slug,
+        label: multiTextValueToSingle(row.value.label),
+      };
+    });
+  };
+  async loadCanvasItems() {
+    try {
+      await Promise.all([this.#loadCanvases()]);
+    } catch (error) {
+      throw error;
+    }
+  }
+
+  toJSON() {
+    return {
+      id: this.#id,
+      slug: this.#slug,
+      label: this.#label,
+      summary: this.#summary,
+      ocrPdf: this.#ocrPdf,
+      canvases: this.#canvases,
+      type: this.#type,
+      freezeParamaters: this.#freezeParamaters,
+    };
+  }
   // returns the labels of a list of manifest ids
   static async basicLookup(manifestIds) {
     if (manifestIds.length === 0) return {};
